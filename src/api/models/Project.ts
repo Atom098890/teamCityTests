3455ec5523b79c3d42e73c4d283666d71f12265b
doroshenko <doroshenko@tutu.tech>
2024-11-23 16:58:16 +0800

    added faker, specification

--- src/api/models/Project.ts
+++ src/api/models/Project.ts
@@ -1,10 +1,5 @@
-import {fakerEN} from "@faker-js/faker";
-
 export class Project {
     readonly id: string;
     readonly name: string;
     readonly locator: string = "_Root";
-
-    constructor() {
-    }
 }
